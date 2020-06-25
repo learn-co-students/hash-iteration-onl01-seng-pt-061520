11e5737512dc51b40f7bcb32974da79c9e26d0c0
Cameron Hudson <camnn3@yahoo.com>
2020-06-25 17:40:44 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,5 +9,8 @@
 def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
-    if 
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+    return winner
 end
