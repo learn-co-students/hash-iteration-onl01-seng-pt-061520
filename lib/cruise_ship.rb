3214a9dc55870f5e3bbcc248868863801744818c
ejixxer999 <lehman.ethan2@gmail.com>
2020-06-27 08:04:06 -0400

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,16 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+ passengers = {
+ suite_a: "Amanda Presley", 
+ suite_b: "Seymour Hoffman", 
+ suite_c: "Alfred Tennyson", 
+ suite_d: "Charlie Chaplin", 
+ suite_e: "Crumpet the Elf"
+ }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  passengers.each do |suite, passengers|
+    if suite == :suite_a && passengers.start_with?("A")
+      return passengers
+    end
+  end
 end
+
